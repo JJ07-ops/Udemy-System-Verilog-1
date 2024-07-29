97469a380575dc1ee113d5ffab15326d0a693c08
JJ07-ops <118655979+JJ07-ops@users.noreply.github.com>
2024-07-29 16:22:54 +0800

    Create Assignment-A23.sv
    first commit

---
+++ Assignment-A23.sv
@@ -0,0 +1,30 @@
+//Assignment agenda
+//Write a code to generate a 9MHz square waveform for the signal sclk. 
+//Assume timescale with 1nsec time-unit and 3 digit precision.
+
+`timescale 1ns/1ps
+
+module tb(); //period: 40 nsec and duty cycle: 0.4
+  
+  //declare clock variable
+  reg sclk = 0;
+  real frequency = 9000000;
+  real period = (1/frequency) * (1 * 1000000000);
+  real delay = period/2;
+  
+  //main part of code
+  always #delay sclk = ~sclk;
+  
+  //dump
+  initial begin;
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+  
+  //force finish
+  initial begin
+    #200
+    $finish();
+  end
+  
+endmodule
