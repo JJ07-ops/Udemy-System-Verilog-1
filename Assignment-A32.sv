f6abc9cef8ad31b1574068469b6c0bd7dbd9d6bb
JJ07-ops <118655979+JJ07-ops@users.noreply.github.com>
2024-07-30 10:12:09 +0800

    Update Assignment-A32.sv

--- Assignment-A32.sv
+++ Assignment-A32.sv
@@ -1,6 +1,8 @@
 /*Assignment agenda:
 
-Create an array capable of storing 10 elements of an unsigned integer. Initialize all the 10 elements to a value equal to the square of the index of that element. for e.g. first element has index no. 0 so initialize it to 0, the second element has index no. 1 so initialize it to 1, the third element has index no. 2 so initialize it to 4, and so on. Verify the code by sending values of all the elements on Console.
+Create an array capable of storing 10 elements of an unsigned integer. Initialize all the 10 elements to a value equal to the square of the index of that element. 
+for e.g. first element has index no. 0 so initialize it to 0, the second element has index no. 1 so initialize it to 1, the third element has index no. 2 so initialize it to 4, and so on. 
+Verify the code by sending values of all the elements on Console.
 
 
 
