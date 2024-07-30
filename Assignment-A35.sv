82d211443530a24a72d1bfa19aeeb8f46f12f569
JJ07-ops <118655979+JJ07-ops@users.noreply.github.com>
2024-07-30 10:11:25 +0800

    Update Assignment-A35.sv

--- Assignment-A35.sv
+++ Assignment-A35.sv
@@ -1,6 +1,8 @@
 /*Assignment agenda:
 
-Create a Fixed-size array capable of storing 20 elements. Add random values to all the 20 elements by using $urandom function. Now add all the elements of the fixed-size array to the Queue in such a way that the first element of the Fixed-size array should be the last element of the Queue. Print all the elements of both fixed-size array and Queue on Console.
+Create a Fixed-size array capable of storing 20 elements. Add random values to all the 20 elements by using $urandom function. 
+Now add all the elements of the fixed-size array to the Queue in such a way that the first element of the Fixed-size array should be the last element of the Queue. 
+Print all the elements of both fixed-size array and Queue on Console.
 
 Student note: the question is unclear, therefore it is assumed that the elements in the queue has a reversed order compared to the fixed array.
 
