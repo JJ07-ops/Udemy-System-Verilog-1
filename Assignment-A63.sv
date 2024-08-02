a9b58beff6934163821ae27ee5c5f36189ae44f7
JJ07-ops <118655979+JJ07-ops@users.noreply.github.com>
2024-08-02 15:29:28 +0800

    Update Assignment-A63.sv

--- Assignment-A63.sv
+++ Assignment-A63.sv
@@ -1,6 +1,7 @@
 /*Assignment agenda:
 
-For the Generator code mentioned in the Instruction tab, expected values for variable a ranges from 0 to 8, variable b ranges from 0 to 5. Also, add logic to store the number of times randomization failed. Print values of variables during each iteration and error count after generating 20 random values for a and b. Add Constraint Code and Testbench top code to  the Design.
+For the Generator code mentioned in the Instruction tab, expected values for variable a ranges from 0 to 8, variable b ranges from 0 to 5. Also, add logic to store the number of times randomization failed. 
+Print values of variables during each iteration and error count after generating 20 random values for a and b. Add Constraint Code and Testbench top code to  the Design.
 
 Student's note: error count will always remain at 0 unless the code is modified to have the ifelse condition to check for the range and the constraint is removed
 
