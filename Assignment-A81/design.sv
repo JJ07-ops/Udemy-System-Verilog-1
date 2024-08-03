97f4aa4427cc4afdd67f9d7255f56ddfc9672709
JJ07-ops <118655979+JJ07-ops@users.noreply.github.com>
2024-08-03 17:13:00 +0800

    Create design.sv

---
+++ Assignment-A81/design.sv
@@ -0,0 +1,14 @@
+//design code given in the question
+module top
+(
+  input clk,
+  input [3:0] a,b,
+  output reg [7:0] mul
+);
+  
+  always@(posedge clk)
+    begin
+     mul <= a * b;
+    end
+  
+endmodule
